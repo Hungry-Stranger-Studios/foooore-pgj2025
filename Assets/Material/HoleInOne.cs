85a27813261058111cf836d3c71634c4e3084df7
Cody Casselman <cody.casselman@ucalgary.ca>
2025-01-27 14:24:58 -0700

    (FIX) HoleLight Bug
    fixed bug in which the light over the hole was not changing when it was hit

--- Assets/Material/HoleInOne.cs
+++ Assets/Material/HoleInOne.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class HoleInOne : MonoBehaviour
 {
     [SerializeField] Color winColor;
-   void OnTriggerEnter(Collider collider){
+    private Renderer holeLight;
+   
+    private void Awake()
+    {
+        holeLight = GetComponent<Renderer>();
+    }
+    void OnTriggerEnter(Collider collider){
     if(collider.CompareTag("Player")){
         Debug.Log("Hole in one!");
-        GetComponent<Renderer>().material.color = winColor;
+        holeLight.material.color = winColor;
 
     }
    }
