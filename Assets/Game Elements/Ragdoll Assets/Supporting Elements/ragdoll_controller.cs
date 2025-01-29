d5b135c121eec25a7644d4afe20f3ff909da6c48
Evan Mann <ejmann900@gmail.com>
2025-01-28 19:43:11 -0700

    Cleaned the ragdoll controller code

--- Assets/Game Elements/Ragdoll Assets/Supporting Elements/ragdoll_controller.cs
+++ Assets/Game Elements/Ragdoll Assets/Supporting Elements/ragdoll_controller.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class ragdoll_controller : MonoBehaviour
 {
-    [SerializeField] float ragdollPushForce;
-    [SerializeField] float upwardsRicochetForce;
+    [SerializeField] float ragdollPushForce;        // Force applied to the ragdoll upon impact
+    [SerializeField] float upwardsRicochetVelocity; // Vertical velocity given to the ball after hitting the ragdoll
 
-    bool ragdoll_hit = false;
+    bool ragdoll_hit = false;   // Whether or not the ragdoll has been struck
 
+    // Method called by the limbs of the ragdoll when a collision is detected. 
+    // Passes the collision information to get the info about the ball, and the rigidbody of the limb struck
     public void DetectCollision(Collision col, Rigidbody limb_rb)
     {
+        //If they haven't yet been hit...
         if (!ragdoll_hit) { 
-            ragdoll_hit = true; // Allows a ragdoll to only be hit a single time
-            GetComponent<rd_anim_control>().disableAnimator();  //Turn off animations to allow for ragdoll
 
-
-            Rigidbody[] child_rigidbodies = GetComponentsInChildren<Rigidbody>();
-            Vector3 hitVel = col.rigidbody.velocity; // Find the velocity of the ball
+            ragdoll_hit = true;                                                     // Allows a ragdoll to only be hit a single time
+            Vector3 hitVel = col.rigidbody.velocity;                                // Find the velocity of the ball
+            GetComponent<rd_anim_control>().disableAnimator();                      //Turn off animations to allow for ragdoll
+            Rigidbody[] child_rigidbodies = GetComponentsInChildren<Rigidbody>();   // Get all the rigidbodies of the limbs such that each can have a force applied to it
 
             foreach (Rigidbody child in child_rigidbodies)
             {
-                child.useGravity = true;
-                limb_rb.AddForce(hitVel.normalized * ragdollPushForce, ForceMode.Impulse);   // Apply force in the direction of the velocity, multiplied by the power
+                child.useGravity = true;                                                    // Turn on gravity (its off so they don't accelerate while animated)
+                limb_rb.AddForce(hitVel.normalized * ragdollPushForce, ForceMode.Impulse);  // Apply force in the direction of the velocity, multiplied by the power
             }
             
-            col.rigidbody.velocity = new Vector3(hitVel.x, upwardsRicochetForce, hitVel.z);  //Apply an upwards velocity to the ball
+            col.rigidbody.velocity = new Vector3(hitVel.x, upwardsRicochetVelocity, hitVel.z);  // Apply an upwards velocity to the ball
 
-            SphereCollider ballSpCol = col.gameObject.GetComponent<SphereCollider>();   //Get balls collider
-            ballSpCol.isTrigger = true; //Turn off its collider
-            StartCoroutine(solidifyCollider(ballSpCol));  //Turn it back on after a small delay
+            // Get the ball's collider, turn it off, wait 35ms, then turn it back on
+            // Ensures the ball is not interfered with by the ragdoll right after hitting it
+            SphereCollider ballSpCol = col.gameObject.GetComponent<SphereCollider>();   
+            ballSpCol.isTrigger = true; 
+            StartCoroutine(solidifyCollider(ballSpCol));  
         }
     }
 
